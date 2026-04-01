730e35aa2db448e6768d48f81eb0793e45f5592c
sahanakabothu <160205311+sahanakabothu@users.noreply.github.com>
2026-04-01 10:25:06 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Student {
+    int id;
+    String name;
+
+    Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Student> list = new ArrayList<>();
+
+        list.add(new Student(1, "Sahi"));
+        list.add(new Student(2, "Ravi"));
+
+        for (Student s : list) {
+            System.out.println(s.id + " " + s.name);
+        }
+    }
+}
